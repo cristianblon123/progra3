e138b8cca5e5c66c76f0b3e00a2244c51ad6ed09
cristianblon123 <55192166+cristianblon123@users.noreply.github.com>
2020-06-26 23:46:58 +0200

    Add files via upload

---
+++ programacion3/hito5/scriptVoice.js
@@ -0,0 +1,7 @@
+document.getElementById('hablar').addEventListener("click",()=>{
+    decir(document.getElementById("text").value);
+});
+
+function decir(text){
+    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+}
